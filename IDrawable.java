93650428a199b571d342186b6d3b851e7eba27c2
Sigge Olofsgård <siggeo@chalmers.se>
2026-02-19 12:41:34 +0100

    IDrawable collision + image caching

--- IDrawable.java
+++ IDrawable.java
@@ -1,6 +1,7 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
 
 public interface IDrawable {
     public int getWidth();
@@ -9,13 +10,36 @@ public interface IDrawable {
     public double getY();
     public String getImagePath();
 
-    public default BufferedImage getImage() {
-        try {
-            return ImageIO.read(DrawPanel.class.getResourceAsStream(getImagePath()));
-        } catch (IOException ex)
-        {
-            ex.printStackTrace();
-            return null;
+    public static class ImageCache {
+        private static HashMap<String, BufferedImage> images = new HashMap<>();
+        public static BufferedImage fetchImage(String path) {
+            if (images.containsKey(path))
+                return images.get(path);
+
+            try {
+                images.put(path, ImageIO.read(DrawPanel.class.getResourceAsStream(path)));
+                return images.get(path);
+            } catch (IOException ex)
+            {
+                ex.printStackTrace();
+                return null;
+            }
         }
+    }
+
+    public default BufferedImage getImage() {
+        return ImageCache.fetchImage(getImagePath());
+    }
+
+    public default boolean pointCollision(double x, double y) {
+        return getX() <= x && x <= getX() + getWidth()
+            && getY() <= y && y <= getY() + getHeight();
+    }
+
+    public default boolean overlaps(IDrawable other) {
+        return  pointCollision(other.getX()                     , other.getY())
+        ||      pointCollision(other.getX()+other.getWidth() , other.getY())
+        ||      pointCollision(other.getX()                     , other.getY()+other.getHeight())
+        ||      pointCollision(other.getX()+other.getWidth() , other.getY()+other.getHeight());
     }
 }
